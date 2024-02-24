22afabade6a0b75f47ce2f907d811c5081ffb08c
ahmadalalousi <ahmad.o.alalousi@gmail.com>
2024-02-23 20:24:08 -0500

    making some changes to Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("doing some git checking to see if its set up right");
     }
 }
