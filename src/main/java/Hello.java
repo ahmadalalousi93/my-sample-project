d50909526e52d7d167780db6274c255ef6d22df8
ahmadalalousi <ahmad.o.alalousi@gmail.com>
2024-02-23 20:30:31 -0500

    Checking remote repo

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("doing some git checking to see if its set up right");
+        System.out.println("Checking if remote repo is set up properly");
     }
 }
